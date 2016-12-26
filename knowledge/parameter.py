75d4371c9de827657cbe52eb7399de0bdfcb04d7
linhaobuaa <1257819385@qq.com>
2016-12-26 11:48:56 +0800

    yuankun-1226

--- knowledge/parameter.py
+++ knowledge/parameter.py
@@ -2,6 +2,7 @@
 
 from global_config import *
 
+RUN_TYPE = 0
 
 # 关系类型与后续节点primary key的关系
 rel_node_mapping = {join: "event", organise: "event", discuss: "event", first: "event", other_rel: "event", \
@@ -18,3 +19,28 @@ rel_node_type_mapping = {join: "Event", organise: "Event", discuss: "Event", fir
 
 # 首页显示最多节点数
 index_threshold = 500
+
+
+CHARACTER_TIME_GAP = 7
+
+WEIBO_API_INPUT_TYPE = 1 # 1 mark: need compute sentiment
+
+DAY = 24*3600
+RUN_TEST_TIME  = '2016-03-13'
+Fifteen = 60 * 15
+HALF_HOUR = 1800
+FOUR_HOUR = 3600*4
+MAX_VALUE = 99999999
+WEEK = 7
+WEEK_TIME = 7*24*3600
+MONTH = 30
+MONTH_TIME = 30*24*3600
+EXPIRE_TIME = 8*24*3600
+
+# 敏感词等级评分, string类型
+sensitive_score_dict = {
+            "1": 1,
+            "2": 5,
+            "3": 10
+}
+

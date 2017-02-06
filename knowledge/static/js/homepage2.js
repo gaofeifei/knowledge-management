3ac42191fb6f76a42643750b3edaa31b2f486d08
linhaobuaa <1257819385@qq.com>
2017-02-06 14:38:09 +0800

    ymm

--- knowledge/static/js/homepage2.js
+++ knowledge/static/js/homepage2.js
@@ -1,110 +1,56 @@
 /**
  * Created by Administrator on 2016/11/24.
  */
-~function(){
-    // 路径配置
-    require.config({
-        paths: {
-            echarts: 'http://echarts.baidu.com/build/dist'
-        }
+var myChart = echarts.init(document.getElementById('special'));
+$.get('/index/count_special_event/',function (data) {
+    // 填入数据
+    var data=eval(data);
+    var xx=[],yy=[];
+    for(var i=0;i<data.length;i++){
+        xx.push(data[i][1]);
+    };
+    for(var j=0;j<data.length;j++){
+        yy.push(data[j][0]);
+    };
+    myChart.setOption(option = {
+        title: {
+            // text: '专题名称',
+            // subtext: '数据来自网络'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        // legend: {
+        //     data: yy
+        // },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'value',
+            boundaryGap: [0, 0.01],
+        },
+        yAxis: {
+            type: 'category',
+            data: yy
+        },
+        itemStyle:{
+            normal:{
+                color:'#68ae00',
+            }
+        },
+        series: [
+            {
+                name: '发生次数',
+                type: 'bar',
+                data: xx,
+            },
+        ]
     });
-    require(
-        [
-            'echarts',
-            'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
-        ],
-        function (ec) {
-            // 基于准备好的dom，初始化echarts图表
-            var myChart = ec.init(document.getElementById('special'));
-
-            var option = {
-                title : {
-                    // text: '世界人口总量',
-                    // subtext: '数据来自网络'
-                },
-                tooltip : {
-                    trigger: 'axis'
-                },
-                // legend: {
-                //     data:yy
-                // },
-                // toolbox: {
-                //     show : true,
-                //     feature : {
-                //         mark : {show: true},
-                //         dataView : {show: true, readOnly: false},
-                //         magicType: {show: true, type: ['line', 'bar']},
-                //         restore : {show: true},
-                //         saveAsImage : {show: true}
-                //     }
-                // },
-                calculable : true,
-                xAxis : [
-                    {
-                        type : 'value',
-                        boundaryGap : [0, 0.01],
-                        data:[]
-                    }
-                ],
-                yAxis : [
-                    {
-                        type : 'category',
-                        data : []
-                    }
-                ],
-                series : [
-                    {
-                        name:'事件名称',
-                        type:'bar',
-                        data : []
-                    },
-                    {
-                        name:'数量',
-                        type:'bar',
-                        data:[]
-                    }
-                ]
-            };
-
-            //数据请求
-            $.get('/index/count_special_event/').done(function (data) {
-                // 填入数据
-                var data=eval(data);
-                xx=[];
-                for(var i=0;i<data.length;i++){
-                    xx.push(data[i][1]);
-                }
-                yy=[];
-                for(var i=0;i<data.length;i++){
-                    yy.push(data[i][0]);
-                }
-                myChart.setOption({
-                    xAxis: {
-                        type : 'value',
-                        boundaryGap : [0, 0.01],
-                        data: xx
-                    },
-                    yAxis: {
-                        type : 'category',
-                        data: yy
-                    },
-                    series: [
-                        {
-                            name:'事件名称',
-                            type:'bar',
-                            data : yy
-                        },
-                        {
-                            name:'数量',
-                            type:'bar',
-                            data: xx
-                        }
-                    ]
-                });
-            });
-
-            // 为echarts对象加载数据
-            // myChart.setOption(option);
-        }
-    );
-}();
+});
--- knowledge/static/js/proults2.js
+++ knowledge/static/js/proults2.js
@@ -118,7 +118,7 @@ function guanlianrenwu() {
             }else {
                 mingan=item.sensitive.toFixed(2);
             };
-            if (item.user_tag==''||item.sensitive=='unknown'||item.sensitive=='null'){
+            if (item.user_tag==''||item.user_tag=='unknown'||item.user_tag=='null'){
                 tag='无';
             }else {
                 tag=item.user_tag;
@@ -129,32 +129,35 @@ function guanlianrenwu() {
                 photo=item.photo_url;
             };
             str+='<div class="play">'+
+                '<div class="p_top" style="width: 100%"><img class="play2" src="'+photo+'" alt="">'+
+                '<img class=\'xin\' style="margin-top: -24px" src="/static/image/heart.png">' +
+                '<span class="xingming" style="text-align:center;color: #000;display: block;margin-top: -13px">'+name+'</span>'+
+                '</div>'+
+                '<div class="play23" style="width: 110px;text-align: left;float: left">'+
+                '<a class="renzh1">认证类型:<span title="'+item.topic_string.replace(/&/g,'  ')+'" class="renzh11">'+item.topic_string.replace(/&/g,'  ')+'</span></a>'+
+                '<a class="renzh2">领&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;域:<span title="'+item.topic_string.replace(/&/g,'  ')+'" class="renzh22">'+item.topic_string.replace(/&/g,'  ')+'</span></a>'+
+                '</div>'+
+                '<div style="float: left;width: 110px;margin-left: 10px">' +
+                '<div class="play3" style="text-align: left">'+
+                '<a class="bus1">业务标签：</a>'+
+                '<a class="bus2">'+tag+'</a>'+
+                '</div>'+
                 '<div class="play1">'+
                 '<div class="p11">'+
                 '<span id="uid" style="display: none">'+item.uid+'</span>'+
-                '<span class="xingming" style="color: #000;font-weight: 900;font-size: 18px;margin-left: 15px">'+name+'</span><!--'+
-                '--><img style="margin-left: 15px;" src="/static/image/fensishu.png" alt=""'+
-                'title=\'粉丝数\'><!--'+
-                '--><span class="difang" style="font-size: 8px">'+item.fansnum+'</span><!--'+
-                '--><img class=\'xin\' style="margin-left: 10px;" src="/static/image/heart.png">'+
                 '</div>'+
-                '<div class="p22" style="margin-top: 5px">'+
-                '<img style="margin-left: 10px;" src="/static/image/influence.png" title="影响力">'+
+                '<div class="p22" style="float:left;margin-top: -5px">'+
+                '<div><img src="/static/image/fensishu.png" alt=""'+
+                'title=\'粉丝数\'><!--'+
+                '--><span class="difang" style="font-size: 8px">'+item.fansnum+'</span>'+
+                '<img src="/static/image/mingan.png" title="敏感度">'+
+                '<span class="mingan">'+mingan+'</span></div>'+
+                '<div><img src="/static/image/influence.png" title="影响力">'+
                 '<span class="influence">'+influe+'</span>'+
                 '<img src="/static/image/huoyuedu.png" title="活跃度">'+
-                '<span class="huoyuedu">'+huoyue+'</span>'+
-                '<img src="/static/image/mingan.png" title="敏感度">'+
-                '<span class="mingan">'+mingan+'</span>'+
+                '<span class="huoyuedu">'+huoyue+'</span></div>'+
                 '</div>'+
                 '</div>'+
-                '<img class="play2" src="'+photo+'" alt="">'+
-                '<div class="play23" style="margin-left: 15px;">'+
-                '<a class="renzh1">认证类型:<span class="renzh11">'+item.topic_string.replace(/&/g,'  ')+'</span></a>'+
-                '<a class="renzh2">领&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;域:<span class="renzh22">'+item.topic_string.replace(/&/g,'  ')+'</span></a>'+
-                '</div>'+
-                '<div class="play3" style="display:block;margin-top: 10px;vertical-align:bottom;padding-left: 15px">'+
-                '<a class="bus1">业务标签：</a>'+
-                '<a class="bus2">'+tag+'</a>'+
                 '</div>'+
                 '<!--<div class="play4">-->'+
                 '<!--<p class="shuoming">-->'+
@@ -213,11 +216,11 @@ function guanlianrenwu() {
         $.each($("#people .play"),function (index,item) {
             $(item).hover(function () {
                 $(item).find(".play5").css({
-                    "-webkit-transform":"translateY(-40px)",
-                    "-moz-transform":"translateY(-40px)",
-                    "-ms-transform":"translateY(-40px)",
-                    "-o-transform":"translateY(-40px)",
-                    "transform":"translateY(-40px)",
+                    "-webkit-transform":"translateY(0px)",
+                    "-moz-transform":"translateY(0px)",
+                    "-ms-transform":"translateY(0px)",
+                    "-o-transform":"translateY(0px)",
+                    "transform":"translateY(0px)",
                 })
             },function () {
                 $(item).find(".play5").css({
@@ -233,18 +236,18 @@ function guanlianrenwu() {
             var changecolorq=1;
             $(item).find(".play5").on('click',function(){
                 if (changecolorq==1) {
-                    $(this).parent('.play').css({backgroundColor:'#09F'});
+                    $(this).parent('.play').find('.xingming').css({color:'red'});
                     $(this).find('a').text('取消群体探索');
                     changecolorq=2;
                     $('#join4').modal("show");
                 } else {
-                    $(this).parent('.play').css({backgroundColor:'#d2dcf7'});
+                    $(this).parent('.play').find('.xingming').css({color:'#000'});
                     $(this).find('a').text('加入群体探索');
                     changecolorq=1;
                 }
             });
         });
-        var heart=$(".play .play1 .p11 .xin");
+        var heart=$(".play .xin");
         $.each(heart,function(index,item){
             var chan=1;
             $(item).on('click',function(){
@@ -320,23 +323,24 @@ function baohanshijian() {
                 shuoming=item.user_tag;
             };
             function getLocalTime(nS) {
-                return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,18)
+                return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,10)
             };
             str+='<div class="play">'+
-                '<div class="play1">'+
-                '<div class="p11">'+
-                '<span class="xingming" style="color: #000;font-weight: 900;font-size: 18px">'+item.name+'</span><!--'+
-                '--><img src="/static/image/dingwei.png" title="位置"><!--'+
-                '--><span class="difang" style="font-size: 8px">'+weizhi+'</span><!--'+
-                '--><img class="xin" src="/static/image/heart.png" alt="">'+
-                '</div>'+
+                '<div class="play1" style="float:left;">'+
+                '<div class="p11" style="text-align: left;padding-left: 30px">'+
+                '<span class="xingming" title="'+item.name+'" ' +
+                'style="display:block;color: #fff;font-weight: 900;font-size: 18px;width: 80px;white-space: nowrap;overflow: hidden;text-overflow:ellipsis;">' +item.name+'</span>'+
                 '<div class="p22">'+
-                '<span class="fasheng" style="font-weight: bold">发生时间：</span>'+
+                '<span class="fasheng" style="width: 80px">发生时间：</span>'+
                 '<span class="riqi">'+getLocalTime(item.start_ts)+'</span>'+
                 '</div>'+
                 '</div>'+
-                '<img class="play2" src="/static/image/xuyuyu.png" alt="">'+
-                '<div class="play3" style="display: inline-block;margin-top: 10px;vertical-align:bottom;">'+
+                '</div>'+
+                '<div style="float:left;margin: 30px 0 0 30px"><div style="display: inline-block"><img src="/static/image/dingwei.png" title="位置"><!--'+
+                '--><span class="difang" style="font-size: 8px">'+weizhi+'</span><!--'+
+                '--><img class="xin" src="/static/image/heart.png" alt=""></div>'+
+                '<img class="play2" src="/static/image/xuyuyu.png" alt=""></div>'+
+                '<div class="play3" style="width: 103px;display: inline-block;margin: 10px 0 0 30px;vertical-align:bottom;">'+
                 '<a class="bus1">业务标签：</a>'+
                 '<a class="bus2">'+biaoqian+'</a>'+
                 '</div>'+
@@ -426,13 +430,13 @@ function baohanshijian() {
             var changecolorq=1;
             $(item).find(".play5").on('click',function(){
                 if (changecolorq==1) {
-                    $(this).parent('.play').css({backgroundColor:'#09F'});
-                    $(this).find('a').text('取消群体探索');
+                    $(this).parent('.play').find('.xingming').css({color:'red'});
+                    $(this).find('a').text('取消专题');
                     changecolorq=2;
                     $('#join4').modal("show");
                 } else {
-                    $(this).parent('.play').css({backgroundColor:'#d2dcf7'});
-                    $(this).find('a').text('加入群体探索');
+                    $(this).parent('.play').find('.xingming').css({color:'#000'});
+                    $(this).find('a').text('加入专题');
                     changecolorq=1;
                 }
             });
